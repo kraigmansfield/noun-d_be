6983264289b80184b85fca9901f132c29066e13f
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-30 19:48:02 -0800

    added pin routes

--- routes/pins.js
+++ routes/pins.js
@@ -3,12 +3,31 @@ const router = require('express').Router();
 
 const Pin = require('../models/Pin');
 
+//Add a pin
 router.post('/', async(req,res) => {
     const newPin = new Pin(req.body)
 
     try{
         const savedPin = await newPin.save()
+        res.status(200).json(savedPin)
+        console.log("Success! Pin added to DB !")
     }catch(err){
-
+        console.log("Failed! Did not add pin to DB !")
+        res.status(500).json(err)
     }
 })
+
+//Get all pins
+router.get('/', async(req,res) => {
+
+    try{
+        const pins = await Pin.find()
+        console.log("Found all pins!")
+        res.status(200).json(pins)
+    }catch(err){
+        console.log("An error occurred")
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt')
+
+// Register a user
+router.post('/register', async(req,res) => {
+    
+})
+
+// Login as a user
