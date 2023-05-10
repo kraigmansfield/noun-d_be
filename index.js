b18fbeb30dad4e52156ee62fe5644ad6a60990c7
kraigmansfield <Kraig.mansfield@gmail.com>
2023-05-10 12:53:12 -0800

    heroku updates

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/noun-d_be',
         console.log('[SUCCESS] Mongo Connected')
     })
 
+app.use(cors())
 app.use('/api/pins',pinRoute)
 app.use('/api/users',userRoute)
-app.use(cors())
 
 
 app.listen(process.env.PORT || 7800, () => {
