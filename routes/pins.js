6d5134e2f2d158ebebacd6915b0213a5fe68d21b
kraigmansfield <Kraig.mansfield@gmail.com>
2023-05-25 13:11:55 -0800

    heoku updates

--- routes/pins.js
+++ routes/pins.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const Pin = require('../models/Pin');
 
 //Add a pin
-router.post('https://nound.herokuapp.com/api/pins/', async(req,res) => {
-// router.post('/', async(req,res) => {
+// router.post('https://nound.herokuapp.com/api/pins/', async(req,res) => {
+router.post('/', async(req,res) => {
     const newPin = new Pin(req.body)
 
     try{
@@ -17,8 +17,8 @@ router.post('https://nound.herokuapp.com/api/pins/', async(req,res) => {
 })
 
 //Get all pins
-router.get('https://nound.herokuapp.com/api/pins/', async(req,res) => {
-// router.get('/', async(req,res) => {
+// router.get('https://nound.herokuapp.com/api/pins/', async(req,res) => {
+router.get('/', async(req,res) => {
 
     try{
         const pins = await Pin.find()
